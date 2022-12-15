5f1f64c83a8da60a2a511a52c2779b917421a4c5
sourabh1392 <sourabhpatil1167@gmail.com>
2022-12-15 22:37:22 +0530

    Done

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,27 +1,26 @@
 const urlModel = require("../models/urlModel")
 const validator = require("validator")
 const shortid = require("shortid")
-// const redis = require("redis")
-// const { promisify } = require("util")
+const redis = require("redis")
+const { promisify } = require("util")
 const baseUrl = "http://localhost:3000/"
-const {SET_ASYNC,GET_ASYNC}=require("../redis/redis")
 
-// const redisClient = redis.createClient(
-//     17269,
-//     "redis-17269.c264.ap-south-1-1.ec2.cloud.redislabs.com",
-//     { no_ready_check: true }
-// );
+const redisClient = redis.createClient(
+    17269,
+    "redis-17269.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+);
 
-// redisClient.auth("L9uCuh4Xbc3rXguxyUH9LQUOwl3UlXU1", function (err) {
-//     if (err) throw err;
-// });
+redisClient.auth("L9uCuh4Xbc3rXguxyUH9LQUOwl3UlXU1", function (err) {
+    if (err) throw err;
+});
 
-// redisClient.on("connect", async function () {
-//     console.log("Connected to Redis..");
-// });
+redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+});
 
-// const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
-// const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient)
+const GETEX_ASYNC = promisify(redisClient.GETEX).bind(redisClient)
 
 const createUrl = async function (req, res) {
     try {
@@ -35,24 +34,18 @@ const createUrl = async function (req, res) {
         if (!validator.isURL(longUrl)) {
             return res.status(400).send({ status: false, message: "Not a valid url" })
         }
-        let cachedLongUrl=await GET_ASYNC(`${longUrl}`)
-        console.log(cachedLongUrl)
+        let cachedLongUrl=await GETEX_ASYNC(`${longUrl}`)
         let Link=JSON.parse(cachedLongUrl)
-        console.log(Link)
         if(Link){
             return res.status(200).send({ longUrl: Link.longUrl, urlCode: Link.urlCode, shortUrl: Link.shortUrl })
         }
-        // const checkUrl = await urlModel.findOne({ longUrl: longUrl })
-        // if (checkUrl) {
-        //     return res.status(400).send({ status: false, message: "LongUrl already used" })
-        // }
         const urlCode = shortid.generate(longUrl);
         const shortUrl = baseUrl + urlCode;
 
         const url = { longUrl: longUrl, urlCode: urlCode, shortUrl: shortUrl };
 
         const Data = await urlModel.create(url)
-        await SET_ASYNC(`${longUrl}`, JSON.stringify(Data))
+        await SETEX_ASYNC(`${longUrl}`,86400, JSON.stringify(Data))
         return res.status(201).send({ status: true, data: { longUrl: Data.longUrl, shortUrl: Data.shortUrl, urlCode: Data.urlCode } });
     }
     catch (err) {
@@ -69,7 +62,7 @@ const getUrl = async (req, res) => {
         if (!shortid.isValid(urlCode)) {
             return res.status(400).send({ status: false, message: "Please enter valid urlCode" })
         }
-        let cachedUrl = await GET_ASYNC(`${req.params.urlCode}`)
+        let cachedUrl = await GETEX_ASYNC(`${req.params.urlCode}`)
         
         let objCache = JSON.parse(cachedUrl)
         
@@ -81,7 +74,7 @@ const getUrl = async (req, res) => {
             if (!presenturl) {
                 return res.status(404).send({ status: false, message: "Urlcode is not valid" })
             }
-            await SET_ASYNC(`${urlCode}`, JSON.stringify(presenturl))
+            await SETEX_ASYNC(`${urlCode}`,86400, JSON.stringify(presenturl))
             return res.status(302).redirect(presenturl.longUrl)
         }
     }
